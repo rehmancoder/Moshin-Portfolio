3524c62cde9927e3203445cca6bb6bcd22c5db23
rehmancoder <rehmanshabbir68@gmail.com>
2024-02-07 09:10:48 +0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Graphic Designer", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
